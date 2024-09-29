e3715a8689ce9af2b53abc1c69bec36be6156f8a
kindmeplease <116156142+kindmeplease@users.noreply.github.com>
2024-09-29 11:57:10 +0800

    Update frccbot.py

--- frccbot.py
+++ frccbot.py
@@ -2,16 +2,7 @@ import discord
 from discord.ext import commands
 import os
 
-# 獲取當前工作目錄
-current_directory = os.getcwd()
-print("Current Directory:", current_directory)
-
-# 列出當前目錄中的文件和文件夾
-files = os.listdir(current_directory)
-print("Files and directories:", files)
-
-# 獲取環境變量
-TOKEN = os.environ.get('DSISCORD_TOKEN')
+TOKEN = os.environ.get('DISCORD_TOKEN')  # 獲取環境變量
 
 intents = discord.Intents.default()
 intents.message_content = True
